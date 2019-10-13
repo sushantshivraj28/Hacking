636a59fc314a4150c72a95c4bcb0f768ba4637de
Sushant Shivraj <56505652+sushantshivraj28@users.noreply.github.com>
2019-10-13 16:57:31 +0530

    Update mac_changer_simple.py

--- mac_changer_simple.py
+++ mac_changer_simple.py
@@ -6,10 +6,10 @@ interface = input("Enter your interface name : ")
 
 new_mac = input("Enter the 12 digit hexadecimal mac address :")
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call(["ifconfig" , interface  "down"])
 
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac , shell=True)
+subprocess.call(["ifconfig" , interface , "hw" , "ether" , new_mac ])
 
-subprocess.call("ifconfig " + interface + " up" ,shell=True )
+subprocess.call(["ifconfig" , interface , "up"])
 
-subprocess.call("ifconfig " + interface  , shell= True)
+subprocess.call(["ifconfig" , interface])
